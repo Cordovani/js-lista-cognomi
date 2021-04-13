992677647ce606fdcdcf322db2ab9e0cc37be96d
Cordovani <lor.cordovani@gmail.com>
2021-04-13 14:19:22 +0200

    creazione array e push dell'input

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 // prendiamo l'input dell'utente
 var cognomeUtente = prompt("inserire il proprio cognome");
 
-// controlli sull'input
-
 // creiamo l'array presistente cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+cognomiUtenti = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
+
+console.log("il cognome inserito è: " + cognomeUtente);
+console.log(cognomiUtenti);
+
+// controlli sull'input
+if(!isNaN(cognomeUtente)){
+    console.log("si prega di inserire un cognome umano");
+} else {
+// ramo con l'input valido 
+// inserisco l'input nell'array
+cognomiUtenti.push(cognomeUtente);
+
+}
+
+console.log(cognomiUtenti);
+
 
 // ordiniamo la lista alfabeticamente
 
