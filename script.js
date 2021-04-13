40fe11ecfdf4409e04cc28ee48ceb8b0ac325c30
Cordovani <lor.cordovani@gmail.com>
2021-04-13 14:06:52 +0200

    creazione struttura e input utente

---
+++ script.js
@@ -0,0 +1,10 @@
+// prendiamo l'input dell'utente
+var cognomeUtente = prompt("inserire il proprio cognome");
+
+// controlli sull'input
+
+// creiamo l'array presistente cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+
+// ordiniamo la lista alfabeticamente
+
+// stampiamo l'output di nome e posizione "umana"
