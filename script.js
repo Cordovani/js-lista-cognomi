f0a8c993ed10a53488d74660531808542bd39f38
Cordovani <lor.cordovani@gmail.com>
2021-04-14 09:46:28 +0200

    implementazione di uppercase

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
 // prendiamo l'input dell'utente
-var cognomeUtente = prompt("inserire il proprio cognome");
+var cognUtente = [];
+cognUtente = prompt("inserire il proprio cognome");
 
 // creiamo l'array presistente cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
 cognomiUtenti = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
 
-console.log("il cognome inserito è: " + cognomeUtente);
+console.log("il cognome inserito è: " + cognUtente);
 console.log(cognomiUtenti);
 
 // controlli sull'input
-if(!isNaN(cognomeUtente)){
+if (!isNaN(cognUtente)) {
     console.log("si prega di inserire un cognome umano");
-} else {
-// ramo con l'input valido 
-// inserisco l'input nell'array
-cognomiUtenti.push(cognomeUtente);
 
+} else {
+    // ramo con l'input valido 
+    // converto la prima lettera in maiuscolo per usare .sort()
+    // cognUtente.charAt(0) = cognUtente.charAt(0).toUpperCase();
+    // charAt(0) restituisce il carattere all'indice specificato e slice(1) l'array a partire dall'indice indicato.
+     cognUtente = cognUtente.charAt(0).toUpperCase() + cognUtente.slice(1);
+    
+    // inserisco l'input nell'array
+    cognomiUtenti.push(cognUtente);
+
+    // ordiniamo la lista alfabeticamente
+    // proviamo con sort() di default ordina alfabeticamente ascendente, anche con un array di appoggio mette le minuscole dopo le maiuscole
+    cognomiUtenti.sort();
 }
 
 console.log(cognomiUtenti);
-
-
-// ordiniamo la lista alfabeticamente
 
 // stampiamo l'output di nome e posizione "umana"
